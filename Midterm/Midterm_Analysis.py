17489e9f09fb2b6b26859f784a8f955e5da703c4
Amey Kelekar <ameykelekar@Ameys-MacBook-Air.local>
2016-11-01 00:30:27 -0400

    Amey Kelekar Midterm

---
+++ Midterm/Midterm_Analysis.py
@@ -0,0 +1,254 @@
+import csv
+import glob
+import os
+from collections import OrderedDict
+import operator
+from Midterm_StackOver_DataCollection import date_path
+
+path = date_path
+os.chdir(path)
+print(path)
+
+def Analysis_Top_Questions_Weightage():
+    dict1 = {}
+    for file in glob.glob(path+'/ab*.csv', recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            with open('output_top_questions.csv', 'w') as csvoutput:
+                writer = csv.writer(csvoutput, lineterminator='\n')
+                
+                all = []
+                row = next(reader)
+                row.append('Weightage')
+                all.append(row)
+                writer.writerow(all[0])
+                
+                for row in reader:
+                    row.append((int(row[2])*2) + (int(row[3])*5) + (int(row[4])*10))
+                    all.append(row)
+                    
+                sortedData = all[1:]
+                sortedlist = sorted(sortedData, key=lambda row: row[-1], reverse=True)
+                writer.writerows(sortedlist)
+                
+                for row in sortedData:
+                    dictList = {'title':row[-4],'weightage':(int(row[2])*2) + (int(row[3])*5) + (int(row[4])*10)}
+                    dict1[row[-6]] = dictList
+
+            csvoutput.close()
+    newDict = OrderedDict(sorted(dict1.items(), key=lambda kv: kv[1]['weightage'], reverse=True)[:1])
+    print("Top Question is ")
+    for key, value in newDict.items():
+        print('{} \t----->\t\t {}'.format(key,value))
+
+def Analysis_Top_User_Per_Topic():
+    Tags = []
+    for file in glob.glob(path+'/tags_*.csv', recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip the headers
+            for row in reader:
+                if row[2] not in Tags:
+                    Tags.append(row[2])
+
+    print(Tags)
+
+    for data in Tags:
+        userIds =[]
+        for file in glob.glob(path+'/tags_*.csv', recursive=True):
+            with open(file, newline='') as f:
+                reader = csv.reader(f)
+                next(reader, None)  # skip the headers
+                for row in reader:
+                    if row[2] == data:
+                        userIds.append(row[0])
+    
+        dict2 = {}
+        for file in glob.glob(path+'/questions_*.csv', recursive=True): 
+            with open(file, newline='') as f:
+                reader = csv.reader(f)
+                all = []
+                test = ["user_id","user_type","display_name","link","reputation"]
+                all.append(test)
+                next(reader, None)  # skip the headers
+                for row in reader:
+                    if row[0] in userIds:
+                        all.append(row[:5])
+                def ensure_dir(f):
+                    if not os.path.exists(f):
+                        os.makedirs(f)
+                ensure_dir('output_top_users')
+            
+                with open('output_top_users/'+data+'.csv', 'w') as csvoutput:
+                    writer = csv.writer(csvoutput, lineterminator='\n')
+                    writer.writerow(all[0])
+                    sortedData = all[1:]
+                    sortedlist = sorted(sortedData, key=lambda row: row[-1], reverse=True)
+                    writer.writerows(sortedlist)
+                
+                    for row in sortedData:
+                        dictList = {'name':row[2],'link':row[3], 'reputation': row[4]}
+                        dict2[row[0]] = dictList
+    
+        newDict = OrderedDict(sorted(dict2.items(), key=lambda kv: kv[1]['reputation'], reverse=True)[:1])
+        print("Top User for %s is: " % data)
+        for key, value in newDict.items():
+            print('{} \t----->\t\t {}'.format(key,value))
+                
+        csvoutput.close() 
+
+def Analysis_Popular_Badge_User():
+    Badges = []
+    for file in glob.glob(path+'/badge_*.csv', recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip the headers
+            for row in reader:
+                if row[-2] not in Badges:
+                    Badges.append(row[-2])
+
+    def ensure_dir(f):
+        if not os.path.exists(f):
+            os.makedirs(f)
+    ensure_dir('output_Badges')
+
+    badgefn = "output_Badges/badge_users_count.csv"
+    badgeFile = open(badgefn,"w")
+    badge_file = csv.writer(badgeFile)
+    badge_file.writerow(["Badge_Name","User_Ids","Count"])
+
+    badgesData = []
+    for data in Badges:
+        userIdCount = 0
+        userIds = []
+        for file in glob.glob(path+'/badge*.csv', recursive=True):
+            with open(file, newline='') as f:
+                reader = csv.reader(f)
+                next(reader, None)  # skip the headers
+                for row in reader:
+                    if row[-2] == data:
+                        userIds.append(row[0])
+                        userIdCount += 1
+                badgesData.append([data,userIds,userIdCount])
+
+    sortedlist = sorted(badgesData, key=lambda row: row[-1], reverse=True)
+    badge_file.writerows(sortedlist)
+    badgeFile.close()
+
+    newList = sortedlist[:1]
+    print(newList)
+    print("Most Popular Badge among user is '%s' and count is '%s'" % (newList[0][0],newList[0][2]))
+
+def Analysis_User_Question_Downvoted_Most():
+    dict3 = {}
+    for file in glob.glob(path+'/questions*.csv', recursive=True): 
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            all = []
+            test = ["user_id","display_name","question_id","title","down_vote_count"]
+            all.append(test)
+            next(reader, None)  # skip the headers
+        
+            for row in reader:
+                test1 =[row[0],row[2],row[-5],row[-3],row[-1]]
+                all.append(test1)
+            
+            def ensure_dir(f):
+                if not os.path.exists(f):
+                    os.makedirs(f)
+            ensure_dir('output_down_vote_count')
+        
+            with open('output_down_vote_count/'+'downVoteCount.csv', 'w') as csvoutput:
+                writer = csv.writer(csvoutput, lineterminator='\n')
+                writer.writerow(all[0])
+                sortedData = all[1:]
+                sortedlist = sorted(sortedData, key=lambda row: row[-1], reverse=True)
+                writer.writerows(sortedlist)
+            
+                for row in sortedData:
+                    dictList = {'name':row[1],'question_id':row[2], 'title': row[3], 'down_vote_count': row[4]}
+                    dict3[row[0]] = dictList
+            csvoutput.close()   
+        
+    newDict = OrderedDict(sorted(dict3.items(), key=lambda kv: kv[1]['down_vote_count'], reverse=True)[:1])
+    print("The user whose question have been downvoted the most:")
+    for key, value in newDict.items():
+        print('{} \t----->\t\t {}'.format(key,value))
+
+def Analysis_Tagwise_NoOfQuestion_And_Answer():
+    dict4 = {}
+
+    for file in glob.glob(path+'/questions*.csv', recursive=True): 
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            all = []
+            test = ["question_id","question_link","title","answer_count","tags"]
+            all.append(test)
+            next(reader, None)  # skip the headers
+        
+            for row in reader:
+                test1 =[row[-5],row[-4],row[-3],row[8],row[5][1:-1]]
+                all.append(test1)
+            
+            def ensure_dir(f):
+                if not os.path.exists(f):
+                    os.makedirs(f)
+            ensure_dir('output_answerCounts_tags')
+        
+            with open('output_answerCounts_tags/'+'tagsAndanswerCount.csv', 'w') as csvoutput:
+                writer = csv.writer(csvoutput, lineterminator='\n')
+                writer.writerow(all[0])
+                sortedData = all[1:]
+                sortedlist = sorted(sortedData, key=lambda row: row[-1], reverse=True)
+                writer.writerows(sortedlist)
+        
+            csvoutput.close()
+
+    Tags =[]
+    for file in glob.glob(path+'/output_answerCounts_tags/tagsAndAnswerCount.csv', recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip the headers
+            for row in reader:
+                if row[-1] not in Tags:
+                    Tags.append(row[-1].replace("'", ""))
+    result =[]
+    for tag in Tags:
+        result.append((tag.replace(",", "")).split())
+    print(result)
+    result1 = set(value for tag in result for value in tag)
+    print(result1)
+
+    countfn = "output_answerCounts_tags/question_answer_count.csv"
+    countFile = open(countfn,"w")
+    count_file = csv.writer(countFile)
+    count_file.writerow(["Tag_Name","questions_asked_count","Total_answered_count"])
+
+    countData=[]
+    
+    for data in result1:
+        anwerCount = 0
+        questionCount = 0
+        for file in glob.glob(path+'/output_answerCounts_tags/tagsAndAnswerCount.csv', recursive=True):
+            with open(file, newline='') as f:
+                reader = csv.reader(f)
+                next(reader, None)  # skip the headers
+                for row in reader:
+                    if data in row[-1]:
+                        anwerCount += int(row[-2])
+                        questionCount += 1
+                countData.append([data,questionCount,anwerCount])
+
+    sortedlist = sorted(countData, key=lambda row: row[-1], reverse=True)
+    count_file.writerows(sortedlist)
+    countFile.close()
+
+    newList = sortedlist[:1]
+    print("Most Answered Tag is '%s'" % newList[0][0])
+
+Analysis_Top_Questions_Weightage()
+Analysis_Top_User_Per_Topic()
+Analysis_Popular_Badge_User()
+Analysis_User_Question_Downvoted_Most()
+Analysis_Tagwise_NoOfQuestion_And_Answer()
+
---
+++ Midterm/Midterm_StackOver_DataCollection.py
@@ -0,0 +1,137 @@
+import requests
+import datetime
+import os
+import csv
+import glob
+import simplejson
+import json
+from collections import OrderedDict
+
+url = 'https://api.stackexchange.com/2.2/questions'
+payload = {'key': 'g04eu)PhKDJkZAO2M191DA((', 'order': 'desc', 'sort': 'votes', 'tagged': 'python;pandas','site': 'stackoverflow', 'filter': '!9YdnSHzjM'}
+r = requests.get(url, params=payload)
+
+#We will use the variables day, month, year, hour, minute and second for our output file name#
+now = datetime.datetime.now()
+day = int(now.day)
+month = int(now.month)
+year = int(now.year)
+hour = int(now.hour)
+minute = int(now.minute)
+second = int(now.second)
+
+dir_path = os.getcwd()
+new_path = dir_path + "/Stackoverflow_Search"
+
+def ensure_dir(f):
+    if not os.path.exists(f):
+        os.makedirs(f)
+
+ensure_dir(new_path)
+date_path = new_path + "/%i_%i_%i" % (now.month, now.day, now.year)
+ensure_dir(date_path)
+
+#time_path = date_path + "/%i_%i_%i" % (now.hour, now.minute, now.second)
+#ensure_dir(time_path)
+
+os.chdir(date_path)
+questions = r.json()
+
+if __name__ == '__main__':
+    #name our output file - %i will be replaced by current month, day, year, hour, minute and second
+    outfn = "questions_%i_%i_%i_%i_%i_%i.csv" % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+    f=open(outfn,"w")
+    csv_file=csv.writer(f)
+    csv_file.writerow(["user_id","user_type","display_name","link","reputation","tags","is_answered","view_count","answer_count","score","question_id","question_link","title","creation_date","down_vote_count"])
+    
+    for entry in questions['items']:    
+        csv_file.writerow([entry['owner']['user_id'],entry['owner']['user_type'],entry['owner']['display_name'],entry['owner']['link'],entry['owner']['reputation']
+                           ,entry['tags'],entry['is_answered'],entry['view_count'],entry['answer_count'],entry['score'],entry['question_id']
+                           ,entry['link'],entry['title'],entry['creation_date'],entry['down_vote_count']])
+    f.close()
+
+    userfn = "users_%i_%i_%i_%i_%i_%i.csv" % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+    userFile = open(userfn,"w")
+    user_file = csv.writer(userFile)
+    user_file.writerow(["user_id","user_type","display_name","link","reputation","account_id","bronze_badge","silver_badge","gold_badge"])
+    
+    for file in glob.glob(date_path+"/"+outfn, recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip the headers
+            
+            for row in reader:
+                payload1 = {'key': 'g04eu)PhKDJkZAO2M191DA((', 'order': 'desc', 'sort': 'reputation','site': 'stackoverflow'}
+                url_user_by_id = 'https://api.stackexchange.com/2.2/users/'+row[0]
+                user= requests.get(url_user_by_id, params=payload1)
+                users = user.json()
+                for records in users['items']:
+                    user_file.writerow([records['user_id'],records['user_type'],records['display_name'],records['link'],records['reputation']
+                                        ,records['account_id'],records['badge_counts']['bronze'],records['badge_counts']['silver'],records['badge_counts']['gold']])
+                    
+    userFile.close()
+
+    tagfn = "tags_%i_%i_%i_%i_%i_%i.csv" % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+    tagsFile = open(tagfn,"w")
+    tag_file = csv.writer(tagsFile)
+    tag_file.writerow(["user_id","count","name"])
+    
+    for file in glob.glob(date_path+"/"+outfn, recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip the headers
+            
+            for row in reader:
+                payload2 = {'key': 'g04eu)PhKDJkZAO2M191DA((', 'order': 'desc', 'sort': 'popular','site': 'stackoverflow'}
+                url_user_tags = 'https://api.stackexchange.com/2.2/users/'+row[0]+'/tags'
+                user_tags = requests.get(url_user_tags, params=payload2)
+                usersTags = user_tags.json()
+                for records1 in usersTags['items']:
+                    tag_file.writerow([records1['user_id'],records1['count'],records1['name']])
+    
+    tagsFile.close()
+
+    badgefn = "badge_%i_%i_%i_%i_%i_%i.csv" % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+    badgeFile = open(badgefn,"w")
+    badge_file = csv.writer(badgeFile)
+    badge_file.writerow(["user_id","user_type","display_name","reputation","user_link","badge_id","link","name","rank"])
+    
+    for file in glob.glob(date_path+"/"+outfn, recursive=True):
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            next(reader, None)  # skip the headers
+            
+            for row in reader:
+                payload3 = {'key': 'g04eu)PhKDJkZAO2M191DA((', 'order': 'desc', 'sort': 'name','site': 'stackoverflow'}
+                url_user_badge = 'https://api.stackexchange.com/2.2/users/'+row[0]+'/badges'
+                user_badge = requests.get(url_user_badge, params=payload3)
+                usersBadge = user_badge.json()
+                for records2 in usersBadge['items']:
+                    badge_file.writerow([records2['user']['user_id'],records2['user']['user_type'],records2['user']['display_name']
+                                         ,records2['user']['reputation'],records2['user']['link'],records2['badge_id'],records['link']
+                                         ,records2['name'],records2['rank']])
+    badgeFile.close()
+    
+    with open(outfn, "rt", encoding="UTF-8") as f:
+        r = csv.reader(f)
+        dict2 = {row[0]: row[1:] for row in r}
+        
+    with open(userfn, "rt", encoding="UTF-8") as f:
+        r = csv.reader(f)
+        dict1 = OrderedDict((row[0], row[5:]) for row in r)
+        
+    result = OrderedDict()
+    for d in (dict1, dict2):
+        for key, value in d.items():
+            result.setdefault(key, []).extend(value)
+
+    combinedfn = "ab_combined_%i_%i_%i_%i_%i_%i.csv" % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+            
+    with open(combinedfn, 'w') as f:
+        w = csv.writer(f)
+        for key, value in result.items():
+            w.writerow([key] + value)
+
+os.chdir(dir_path)
+if __name__ == '__main__':
+    exec(open('Midterm_Analysis.py').read())
